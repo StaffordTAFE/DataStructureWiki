aadf6e096af0e1f0367ea11a80e765058ccfac3f
StaffordTAFE <30082213@tafe.wa.edu.au>
2023-07-28 11:45:11 +0800

    Test push

--- WikiForm/WikiForm/Program.cs
+++ WikiForm/WikiForm/Program.cs
@@ -8,15 +8,6 @@ namespace WikiForm
 {
     internal static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new WikiForm());
-        }
+        //TestPush
     }
 }
